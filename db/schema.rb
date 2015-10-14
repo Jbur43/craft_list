adc34a5d2a015217ef6f23192914e681b4fa8762
Jack Burum <jtburum@gmail.com>
2015-10-14 12:17:51 -0400

    removed sqlite and added pg, added sorcery

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008002201) do
+ActiveRecord::Schema.define(version: 20151013181146) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string   "beer_name"
@@ -21,5 +24,15 @@ ActiveRecord::Schema.define(version: 20151008002201) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",            null: false
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
